c0ffe2eb9ac039fb4c0dd28901a782f65dc3b15b
ta <tuananh31020@gmail.com>
2023-08-08 21:49:59 +0700

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { Form, Field } from 'vee-validate'
+import * as Yup from 'yup'
+
+import { useAuthStore } from '@/stores'
+
+const schema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string().required('Password is required')
+})
+
+function onSubmit(values, { setErrors }) {
+  const authStore = useAuthStore()
+  const { username, password } = values
+
+  return authStore.login(username, password).catch((error) => setErrors({ apiError: error }))
+}
+</script>
+
+<template>
+  <el-row class="row-bg" justify="center">
+    <el-col :span="6">
+      <el-card class="box-card">
+        <h2>Login</h2>
+        <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+          <div class="form-group">
+            <label>Username</label>
+            <!-- <el-input name="username" type="text" placeholder="Username" /> -->
+            <Field
+              name="username"
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': errors.username }"
+            />
+            <div class="invalid-feedback">{{ errors.username }}</div>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <Field
+              name="password"
+              type="password"
+              class="form-control"
+              :class="{ 'is-invalid': errors.password }"
+            />
+            <div class="invalid-feedback">{{ errors.password }}</div>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary" :disabled="isSubmitting">
+              <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
+              Login
+            </button>
+          </div>
+          <div v-if="errors.apiError" class="alert alert-danger mt-3 mb-0">
+            {{ errors.apiError }}
+          </div>
+        </Form>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
